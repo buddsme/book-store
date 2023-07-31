0da16db5af1a28617a71942651313a3c542f738d
Le Makhno <lmakhnorylov@gmail.com>
2023-07-31 19:02:37 +0300

    rm comments from buildfile

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,17 @@
 plugins {
-    // Apply the application plugin to add support for building a CLI application in Java.
     application
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
 dependencies {
-    // Use JUnit Jupiter for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
 
-    // This dependency is used by the application.
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
     implementation("com.google.guava:guava:31.1-jre")
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(11))
@@ -30,11 +25,9 @@ tasks.withType<Jar> {
 }
 
 application {
-    // Define the main class for the application.
     mainClass.set("com.andersen.App")
 }
 
 tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }
